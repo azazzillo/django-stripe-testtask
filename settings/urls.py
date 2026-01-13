f76d42194537f9e6d979967948afac93234696ef
azazzillo <mariyash_zhumabekova@mail.ru>
2026-01-13 16:56:27 +0500

    urls

--- settings/urls.py
+++ settings/urls.py
@@ -1,8 +1,9 @@
 # Django
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('item.urls'))
 ]
